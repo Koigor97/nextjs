0adfa55abeab7a199a3426c46f303e3192c8ef04
Koigor97 <turaysamuel101@yahoo.com>
2024-02-07 21:48:50 +0200

    add main header component

---
+++ foodie/components/shared/main-header/main-header.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import MainHeaderBackground from "./main-header-background";
+
+import logoImg from "@/assets/logo.png";
+import styles from "./main-header.module.css";
+import NavLink from "./nav-link";
+
+function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={logoImg} alt="A plate with food" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <NavLink href={"/meals"}>Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href={"/community"}>Foodie's Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default MainHeader;
