9f8c3d8a6a8179c7cec120fb112cb27bad6926a4
Koigor97 <turaysamuel101@yahoo.com>
2024-02-10 23:33:34 +0200

    implement getting the value from a dynamic route using params keyword

--- coffee-stores/app/coffee-store/[id]/page.tsx
+++ coffee-stores/app/coffee-store/[id]/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-export default function Page() {
+export default function Page(props: { params: { id: string } }) {
+  const {
+    params: { id },
+  } = props;
+
+  console.log("id", id);
   return (
     <div>
-      <h1>Dynamic Slug</h1>
+      <h1>Dynamic Route ID: {id} </h1>
     </div>
   );
 }
