08f21cd71e116a56f3c12b1702fd69b0f3b61393
Koigor97 <turaysamuel101@yahoo.com>
2024-02-13 23:27:58 +0200

    add prop paramters to component

--- foodie/components/meals/meal-item.jsx
+++ foodie/components/meals/meal-item.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "./meal-item.module.css";
 
-function MealItem() {
+function MealItem({ title, slug, image, summary, creator }) {
   return (
     <article className={styles.meal}>
       <header>
