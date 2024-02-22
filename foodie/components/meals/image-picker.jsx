fce21c776885874a0e1b0c8f4b1da1e8d3af1cee
Koigor97 <turaysamuel101@yahoo.com>
2024-02-22 23:45:17 +0200

    create a custom image picker button/component

---
+++ foodie/components/meals/image-picker.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useRef, useState } from "react";
+import Image from "next/image";
+import styles from "./image-picker.module.css";
+
+function ImagePicker({ label, name }) {
+  const imageInput = useRef(null);
+  const [pickedImg, setPickedImg] = useState("");
+
+  function handlePickImageClick() {
+    imageInput.current.click();
+  }
+
+  function handleImageChange(e) {
+    // e.preventDefault();
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImg(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={styles.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.controls}>
+        <div className={styles.preview}>
+          {!pickedImg && <p>Select an image</p>}
+          {pickedImg && (
+            <Image src={pickedImg} alt="The choosen image from the user" fill />
+          )}
+        </div>
+        <input
+          className={styles.input}
+          type="file"
+          name={name}
+          id={name}
+          accept="image/png, image/jpeg, image/webp"
+          ref={imageInput}
+          onChange={handleImageChange}
+        />
+        <b
+          className={styles.button}
+          type="button"
+          onClick={handlePickImageClick}
+        >
+          Pick an Image
+        </b>
+      </div>
+    </div>
+  );
+}
+
+export default ImagePicker;
