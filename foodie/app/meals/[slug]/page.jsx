79598a06e8022f27419211059dae09550b0bdef2
Koigor97 <turaysamuel101@yahoo.com>
2024-02-22 22:42:46 +0200

    create mealsDetail page

--- foodie/app/meals/[slug]/page.jsx
+++ foodie/app/meals/[slug]/page.jsx
@@ -1,9 +1,29 @@
-function SomeMeal() {
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import styles from "./page.module.css";
+
+function MealsDetailPage({ params }) {
+  const meal = getMeal(params.slug);
+
   return (
-    <div>
-      <h1>Some Meal Detail</h1>
-    </div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions}>{meal.instructions}</p>
+      </main>
+    </>
   );
 }
 
-export default SomeMeal;
+export default MealsDetailPage;
