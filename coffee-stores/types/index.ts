9ebef86a4ac79c93cc004c0faf0bc0f93c2773ed
Koigor97 <turaysamuel101@yahoo.com>
2024-02-24 20:59:13 +0200

    create index.ts file

---
+++ coffee-stores/types/index.ts
@@ -0,0 +1,12 @@
+export type CoffeeStoreType = {
+  id: string;
+  name: string;
+  imgUrl: string;
+  address: string;
+};
+
+export type MapboxType = {
+  id: string;
+  properties: { address: string };
+  text: string;
+};
