30a7ae28439a1fbf67d658a040f7546581d013e6
Koigor97 <turaysamuel101@yahoo.com>
2024-02-27 03:46:21 +0200

    create dynamic route slug page

---
+++ nextjs-file-base-routing-proj/pages/events/[eventId].jsx
@@ -0,0 +1,39 @@
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy";
+
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/error-alert/error-alert";
+
+function EventDetailPage() {
+  const router = useRouter();
+  const { eventId } = router.query;
+  const { title, description, location, date, image } = getEventById(eventId);
+
+  if (!eventId) {
+    return (
+      <ErrorAlert>
+        <p>No event detail</p>;
+      </ErrorAlert>
+    );
+  }
+
+  return (
+    <>
+      <EventSummary title={title} />
+      <EventLogistics
+        date={date}
+        location={location}
+        image={image}
+        imageAlt={`Photo of ${title} event`}
+      />
+      <EventContent>
+        <p>{description}</p>
+      </EventContent>
+    </>
+  );
+}
+
+export default EventDetailPage;
