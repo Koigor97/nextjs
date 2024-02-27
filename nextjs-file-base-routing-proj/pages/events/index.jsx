6bb3b80e93284c0cff9fdff981a536915447b29b
Koigor97 <turaysamuel101@yahoo.com>
2024-02-27 03:44:52 +0200

    import components and add logic to EventPage component

--- nextjs-file-base-routing-proj/pages/events/index.jsx
+++ nextjs-file-base-routing-proj/pages/events/index.jsx
@@ -1,8 +1,24 @@
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../dummy";
+
+import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/event-search";
+
 function EventsPage() {
+  const events = getAllEvents();
+  const router = useRouter();
+
+  function handleFindEvent(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
-    <div>
-      <h1>All events page</h1>
-    </div>
+    <>
+      <EventSearch handleFindEvent={handleFindEvent} />
+      <EventList events={events} />
+    </>
   );
 }
 
