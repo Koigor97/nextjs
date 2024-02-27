3de5a3dc381c32348933969dfc5ce884f77a04c8
Koigor97 <turaysamuel101@yahoo.com>
2024-02-27 03:46:53 +0200

    create a 'catch all' dynamic slug page

--- nextjs-file-base-routing-proj/pages/events/[...slug].jsx
+++ nextjs-file-base-routing-proj/pages/events/[...slug].jsx
@@ -1,8 +1,67 @@
+import { useRouter } from "next/router";
+
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/results-title/results-title";
+import Button from "../../components/shared/button";
+import ErrorAlert from "../../components/error-alert/error-alert";
+import { getFilteredEvents } from "../../dummy";
+
 function FilteredEventPage() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = +filterData[0];
+  const filteredMonth = +filterData[1];
+
+  if (
+    isNaN(filteredYear) ||
+    isNaN(filteredMonth) ||
+    filteredYear > 2030 ||
+    filteredYear < 2021 ||
+    filteredMonth < 1 ||
+    filteredMonth > 12
+  ) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link={"/events"}>Show all Events</Button>
+        </div>
+      </>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link={"/events"}>Show all Events</Button>
+        </div>
+      </>
+    );
+  }
+
+  const date = new Date(filteredYear, filteredMonth - 1);
+
   return (
-    <div>
-      <h2>Filtered Events</h2>
-    </div>
+    <>
+      <ResultsTitle date={date} />
+      <EventList events={filteredEvents} />
+    </>
   );
 }
 
