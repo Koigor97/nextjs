bff37102a9e807f7cdba9efc580d247caec23eb8
Koigor97 <turaysamuel101@yahoo.com>
2024-02-28 03:41:08 +0200

    implement the use case of static-site-generation using the useStaticProps() function

--- nextjs-data-fetching-practice/pages/index.jsx
+++ nextjs-data-fetching-practice/pages/index.jsx
@@ -1,11 +1,65 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+import Link from "next/link";
+
 function HomePage(props) {
+  const { mr } = props;
+  const { mrs } = props;
+  const { products } = props;
+  const namesOrder = ["firstName", "middleName", "lastName"];
+
   return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
+    <>
+      <ul>
+        {mr.map((name, i) => (
+          <li key={i}>
+            <Link href={`${name[namesOrder[i]]}`}>{name[namesOrder[i]]}</Link>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {mrs.map((name, i) => (
+          <li key={i}>
+            <Link href={`${name[namesOrder[i]]}`}>{name[namesOrder[i]]}</Link>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {products.map((product, i) => (
+          <li key={i}>
+            <Link href={product.id}>{product.description}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummyData.json");
+  // console.log(filePath);
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  // console.log(data);
+  console.log("RE-GENERATING after EVERY 5sec");
+
+  return {
+    props: {
+      mr: [
+        { firstName: "Koigor" },
+        { middleName: "Samuel" },
+        { lastName: "Fogbawa" },
+      ],
+      mrs: [
+        { firstName: "Sarah" },
+        { middleName: "Juliana" },
+        { lastName: "Alpha" },
+      ],
+      products: data.products,
+    },
+    revalidate: 5,
+  };
 }
 
 export default HomePage;
