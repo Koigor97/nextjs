9609d1ecf506e5d54d6b2c500fbac99221a7159a
Koigor97 <turaysamuel101@yahoo.com>
2024-02-29 18:26:15 +0200

    use getServerSideProps() in userProfile jsx

--- nextjs-data-fetching-practice/pages/user-profile.jsx
+++ nextjs-data-fetching-practice/pages/user-profile.jsx
@@ -1,9 +1,13 @@
-function UserProfile() {
+function UserProfile(props) {
   return (
     <div>
-      <h1>USERNAME</h1>
+      <h1>{props.username}</h1>
     </div>
   );
 }
 
 export default UserProfile;
+
+export async function getServerSideProps(context) {
+  return { props: { username: "Fogbawa" } };
+}
