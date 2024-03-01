36e480626cf2d49b657f8510398081b25c364f93
Koigor97 <turaysamuel101@yahoo.com>
2024-03-01 23:55:59 +0200

    add forms

--- nextjs-api-routes-practice/pages/index.jsx
+++ nextjs-api-routes-practice/pages/index.jsx
@@ -1,7 +1,29 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInput = useRef();
+  const feedBackInput = useRef();
+
+  function handleFormSubmission(e) {
+    e.preventDefault();
+    const enteredEmail = emailInput.current.value;
+    const enteredFeedback = feedBackInput.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={handleFormSubmission}>
+        <div>
+          <label htmlFor="email">Your Email address</label>
+          <input type="email" id="email" ref={emailInput} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea id="feedback" rows="5" ref={feedBackInput} />
+        </div>
+        <button>Send feedback</button>
+      </form>
     </div>
   );
 }
