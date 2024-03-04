89d39c8310035e07f05f8fdef0a0e8a06e32abdd
Koigor97 <turaysamuel101@yahoo.com>
2024-03-04 19:11:24 +0200

    add more logic to api-route practice page component

--- nextjs-api-routes-practice/pages/feedback/index.jsx
+++ nextjs-api-routes-practice/pages/feedback/index.jsx
@@ -1,13 +1,54 @@
-function FeedBackPage() {
+import Link from "next/link";
+import { useState } from "react";
+
+import { data } from "../../data/dummyData";
+
+export default function FeedBackPage(props) {
+  const { feedbackData } = props;
+  const [isClicked, setIsClicked] = useState(null);
+  const [theData, setTheData] = useState(null);
+
+  async function handleFetchData(id) {
+    setIsClicked(!false);
+    const res = await fetch(`/api/${id}`);
+    const data = await res.json();
+    console.log(data);
+    setTheData(data);
+  }
+
   return (
-    <ul>
-      {feebackData.map((data) => (
-        <li
-          key={data.id}
-        >{`Title: ${data.title}, Description: ${data.description}, Assignee: ${data.assignee}`}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {feedbackData.map((data) => (
+          <li key={data.id}>
+            {`Title: ${data.title}`}{" "}
+            <button onClick={handleFetchData.bind(null, data.id)}>
+              {isClicked ? "hide details" : "view details"}
+            </button>
+            {isClicked && <FeedbackDetails item={theData} />}
+          </li>
+        ))}
+      </ul>
+      <Link href="/">Go back</Link>
+    </>
   );
 }
 
-export default FeedBackPage;
+export async function getStaticProps() {
+  return {
+    props: {
+      feedbackData: data,
+    },
+  };
+}
+
+function FeedbackDetails(item) {
+  return (
+    <div>
+      <h3>Title: {item.title}</h3>
+      <p>Description: {item.description} </p>
+      <p>Assignee: {item.assignee}</p>
+      <p>Priority: {item.priority}</p>
+    </div>
+  );
+}
