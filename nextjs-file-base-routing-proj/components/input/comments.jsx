a1dca350b39e6cf46940056bcaee56b96242e029
Koigor97 <turaysamuel101@yahoo.com>
2024-03-05 11:09:16 +0200

    add logic to get and post comments

--- nextjs-file-base-routing-proj/components/input/comments.jsx
+++ nextjs-file-base-routing-proj/components/input/comments.jsx
@@ -1,29 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
-import CommentList from './comment-list';
-import NewComment from './new-comment';
-import classes from './comments.module.css';
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
 
-function Comments(props) {
-  const { eventId } = props;
-
+function Comments({ eventId }) {
   const [showComments, setShowComments] = useState(false);
+  const [listOfComment, setListOfComment] = useState([]);
+
+  useEffect(() => {
+    async function getComments() {
+      const res = await fetch(`/api/comments/${eventId}`);
+      const { data } = await res.json();
+      console.log(data);
+      setListOfComment(data);
+    }
+
+    getComments();
+  }, [showComments]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
-  function addCommentHandler(commentData) {
+  async function addCommentHandler(commentData) {
     // send data to API
+    const sentStatus = await fetch(`/api/comments/${eventId}`, {
+      method: "POST",
+      body: JSON.stringify(commentData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const statusData = await sentStatus.json();
+    console.log(statusData);
   }
 
   return (
     <section className={classes.comments}>
       <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
+        {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList commentList={listOfComment} />}
     </section>
   );
 }
