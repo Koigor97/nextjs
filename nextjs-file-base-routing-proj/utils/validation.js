d09e3566e92e5e59045a04695ec81d395e18b101
Koigor97 <turaysamuel101@yahoo.com>
2024-03-05 11:06:44 +0200

    create validation file

---
+++ nextjs-file-base-routing-proj/utils/validation.js
@@ -0,0 +1,16 @@
+export function isEmailValid(data) {
+  const extenstion = [".com", ".org", ".edu"];
+  const providers = ["yahoo", "gmail", "outlook"];
+
+  if (!data || data.length === 0) return false;
+
+  for (let i = 0; i < extenstion.length; i++) {
+    if (
+      data.includes(providers[i]) &&
+      data.includes(extenstion[i]) &&
+      data.includes("@")
+    ) {
+      return true;
+    } else return false;
+  }
+}
