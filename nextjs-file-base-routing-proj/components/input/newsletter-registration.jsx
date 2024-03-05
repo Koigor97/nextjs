17fe1c78d41c9e91e6498f4eb0a748a63759c008
Koigor97 <turaysamuel101@yahoo.com>
2024-03-05 11:13:01 +0200

    add logic to newsLetter component

--- nextjs-file-base-routing-proj/components/input/newsletter-registration.jsx
+++ nextjs-file-base-routing-proj/components/input/newsletter-registration.jsx
@@ -1,27 +1,61 @@
-import classes from './newsletter-registration.module.css';
+import { useRef, useState } from "react";
+
+import styles from "./newsletter-registration.module.css";
+import { isEmailValid } from "../../utils/validation";
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
-    event.preventDefault();
+  const emailReference = useRef(null);
+  const [isError, setIsError] = useState("");
+
+  async function registrationHandler(e) {
+    e.preventDefault();
 
     // fetch user input (state or refs)
+    const enteredEmail = emailReference.current.value;
+    console.log(enteredEmail);
     // optional: validate input
-    // send valid data to API
+    if (isEmailValid(enteredEmail)) {
+      // send valid data to API
+      const status = await fetch("/api/register", {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          thePath: "newsletter.json",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const statusData = await status.json();
+      if (statusData.status === "Success") {
+        setIsError(false);
+      }
+      // console.log(statusData);
+    } else {
+      setIsError(true);
+    }
   }
 
   return (
-    <section className={classes.newsletter}>
+    <section className={styles.newsletter}>
       <h2>Sign up to stay updated!</h2>
       <form onSubmit={registrationHandler}>
-        <div className={classes.control}>
+        <div className={styles.control}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
+            ref={emailReference}
           />
           <button>Register</button>
         </div>
+        {isError === false ? (
+          <p>You are successfully registered</p>
+        ) : (
+          <p>Enter a valid email</p>
+        )}
       </form>
     </section>
   );
