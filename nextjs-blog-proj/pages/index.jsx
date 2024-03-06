e6a3092aa1742df7e4641b6f33179352af987a40
Koigor97 <turaysamuel101@yahoo.com>
2024-03-06 12:46:14 +0200

    add hero and featuredPost components

---
+++ nextjs-blog-proj/pages/index.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/homepage/Hero";
+import FeaturedPosts from "../components/homepage/featured-post";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  );
+}
+
+export default HomePage;
