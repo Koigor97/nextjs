4bbb500cb5ca0d86dcc4e4c908ece8956339d093
Koigor97 <turaysamuel101@yahoo.com>
2024-03-07 03:11:52 +0200

    create a dummyData file

---
+++ nextjs-blog-proj/data/dummyData.js
@@ -0,0 +1,74 @@
+export const DUMMY_POSTS = [
+  {
+    title: "The Benefits of Meditation",
+    image:
+      "https://images.unsplash.com/photo-1610295272575-7376b67b3e96?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    excerpt:
+      "Discover the profound impact meditation can have on your mental and physical well-being.",
+    date: "2024-03-06",
+    slug: "benefits-of-meditation",
+  },
+  {
+    title: "Exploring the World of Virtual Reality",
+    image:
+      "https://images.unsplash.com/photo-1554474051-0256b98c36f8?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    excerpt:
+      "Step into the immersive world of virtual reality and experience the future of technology.",
+    date: "2024-03-07",
+    slug: "exploring-virtual-reality",
+  },
+  {
+    title: "A Beginner's Guide to Web Development",
+    image:
+      "https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    excerpt:
+      "Get started on your journey to becoming a web developer with this comprehensive guide.",
+    date: "2024-03-08",
+    slug: "beginners-guide-web-development",
+  },
+  {
+    title: "Healthy Eating Habits for Busy Professionals",
+    image:
+      "https://images.unsplash.com/photo-1494390248081-4e521a5940db?q=80&w=1406&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    excerpt:
+      "Learn how to maintain a healthy diet even with a hectic schedule, and improve your overall well-being.",
+    date: "2024-03-09",
+    slug: "healthy-eating-habits",
+  },
+  {
+    title: "The Art of Mindfulness",
+    image:
+      "https://images.unsplash.com/photo-1621090483697-29b32b735a73?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    excerpt:
+      "Discover the art of living in the present moment and its profound impact on stress reduction and mental clarity.",
+    date: "2024-03-10",
+    slug: "art-of-mindfulness",
+  },
+  {
+    title: "Mastering JavaScript: Tips and Tricks",
+    image:
+      "https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    excerpt:
+      "Enhance your JavaScript skills with these expert tips and tricks to become a more efficient developer.",
+    date: "2024-03-11",
+    slug: "mastering-javascript-tips",
+  },
+  {
+    title: "The Power of Positive Thinking",
+    image:
+      "https://images.unsplash.com/photo-1531857454108-c65232a962a8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    excerpt:
+      "Explore how cultivating a positive mindset can transform your life and improve your overall happiness.",
+    date: "2024-03-12",
+    slug: "power-of-positive-thinking",
+  },
+  {
+    title: "Effective Communication Strategies for Professionals",
+    image:
+      "https://images.unsplash.com/photo-1544717301-9cdcb1f5940f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    excerpt:
+      "Learn how to communicate effectively in the workplace and build strong professional relationships.",
+    date: "2024-03-13",
+    slug: "effective-communication-strategies",
+  },
+];
