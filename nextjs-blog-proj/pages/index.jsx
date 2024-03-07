1adaa40edf6ec831fd38d496f655fd63f123fa22
Koigor97 <turaysamuel101@yahoo.com>
2024-03-07 03:12:27 +0200

    modify the code

--- nextjs-blog-proj/pages/index.jsx
+++ nextjs-blog-proj/pages/index.jsx
@@ -1,11 +1,15 @@
 import Hero from "../components/homepage/Hero";
 import FeaturedPosts from "../components/homepage/featured-post";
 
+import { DUMMY_POSTS } from "../data/dummyData";
+// title, image, excerpt, date, slug
+const dummyPosts = DUMMY_POSTS.slice(0, 4);
+
 function HomePage() {
   return (
     <>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={dummyPosts} />
     </>
   );
 }
