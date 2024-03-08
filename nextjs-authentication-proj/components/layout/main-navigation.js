09df5b051c7188adc42ffeb1f5099b4fdb2fc46a
Koigor97 <turaysamuel101@yahoo.com>
2024-03-08 23:59:53 +0200

    modify file

--- nextjs-authentication-proj/components/layout/main-navigation.js
+++ nextjs-authentication-proj/components/layout/main-navigation.js
@@ -1,8 +1,12 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { useSession } from "next-auth/client";
 
-import classes from './main-navigation.module.css';
+import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
+  const [session, loading] = useSession();
+  console.log(session, loading);
+
   return (
     <header className={classes.header}>
       <Link href="/">
